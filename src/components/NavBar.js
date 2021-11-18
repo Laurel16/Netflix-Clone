59b3cb3296b4aa1069b37a83d4c242ecea6fbfea
laure leter <laure.leter@gmail.com>
2021-11-18 19:15:38 +0100

    links to DB & home-page style

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>
+          <div className="logo"></div>
+        </li>
+        <li>
+          <a href="/">Home</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default NavBar
+
+
